c3c24c1161a6cb3e3e79d1711d0b2a6e1dcad6ef
mankesh543 <165131137+mankesh543@users.noreply.github.com>
2025-07-03 23:58:34 +0530

    reversed cors

--- src/index.js
+++ src/index.js
@@ -15,14 +15,14 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-
+const FRONTEND_URL=process.env.FRONTEND_URL;
 const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "*",
+    origin: [FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
   })
 );
@@ -30,13 +30,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
